a744eb2a10c465fe35bfc7260743f5f78a562b7a
JHSComp <wgtrcomp9512@gmail.com>
2021-06-15 18:26:00 +0900

    BlackJackMk1

---
+++ vue2-blackjack/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <i class="bi-suit-heart"></i>
+    <BlackJackMain></BlackJackMain>
+  </div>
+</template>
+
+<script>
+import BlackJackMain from './components/test-blackjack.vue'
+export default {
+  name: 'App',
+  components : {
+    BlackJackMain
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue2-blackjack/src/components/blackjack-single-card.vue
@@ -0,0 +1,79 @@
+<template>
+    <b-card :class='classObject' class='mx-2 my-2' style='min-width:18rem; max-width:18rem; max-height:25rem; min-height: 25rem'>
+        <div>
+            <span>{{numPtn}}</span>
+            <div style="height:20rem">
+                <div v-for='(n,idx) in num' :key = 'idx' style = "display:inline-block" class="px-3 py-2">
+                    <b-icon :icon='classSymbol' font-scale='3'></b-icon>
+                </div>
+            </div>
+            <span>{{pattern}}</span>
+        </div>
+    </b-card>
+</template>
+<script>
+
+export default {
+    name : 'BlackJackCard',
+    props : {
+        numPtn : {
+            type : String
+        },
+        pattern : {
+            type : String
+        },
+        num : {
+            type : Number
+        }
+    },
+    data(){
+        return {
+            
+        }
+    },
+    computed : {
+        classObject : function(){
+            return{
+                'club' : this.pattern == 'Clover' ,
+                'heart' : this.pattern == 'Heart' ,
+                'diamond' : this.pattern == 'Diamond' ,
+                'spade' : this.pattern == 'Spade' 
+                }
+        },
+        classSymbol : function(){
+            let {club = false, heart = false, diamond = false, spade = false} = this.classObject;
+            if(club){
+                return 'suit-club'
+            }
+            if(heart){
+                return 'suit-heart'
+            }
+            if(diamond){
+                return 'suit-diamond'
+            }
+            if(spade){
+                return 'suit-spade'
+            }
+        }
+    },
+    
+
+}
+</script>
+<style scoped>
+    .diamond{
+        border : black solid 2px;
+    }
+    .heart{
+        border : red 2px solid;
+        color : red;
+    }
+    .spade{
+        border : grey 2px solid;
+        color : grey;
+    }
+    .club{
+        border : green 2px solid;
+        color : green;
+    }
+</style>
